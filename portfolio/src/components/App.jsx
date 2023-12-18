b197c5c37ca7344c637bf33adee1128e14149f95
kitsosenwedi <boyboykitso@gmail.com>
2023-12-18 12:12:54 +0200

    render troubleshooting

---
+++ portfolio/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+    <h1>Kitso Senwedi</h1>,
+    <p>test</p>,
+    <p>testtttttttttttttttttttttttt</p>
+    )
+}
+
+export default App;
